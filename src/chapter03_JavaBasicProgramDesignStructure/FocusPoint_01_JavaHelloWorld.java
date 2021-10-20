40ed1b32ce6131efb2ab45e77820d1784db6933c
RagnorLixiaomeng <13155328119@163.com>
2021-10-20 17:03:54 +0800

    Complete FocusPoint-01：编写你的第一个Java “Hello World”

--- src/chapter03_JavaBasicProgramDesignStructure/FocusPoint_01_JavaHelloWorld.java
+++ src/chapter03_JavaBasicProgramDesignStructure/FocusPoint_01_JavaHelloWorld.java
@@ -1,4 +1,7 @@
 package chapter03_JavaBasicProgramDesignStructure;
 
 public class FocusPoint_01_JavaHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA!");
+    }
 }
