a7adac6851d4ff459b80125f831ff1f139a5d646
RagnorLixiaomeng <13155328119@163.com>
2021-12-08 15:01:36 +0800

    Complete Task-08_Java Generic Programming Design:FocusPoint_03_Java Define  Generic Method

--- src/com/ragnor/corejava/chapter08_GenericProgramming/FocusPoint_02_JavaDefineSimpleGenericClass.java
+++ src/com/ragnor/corejava/chapter08_GenericProgramming/FocusPoint_02_JavaDefineSimpleGenericClass.java
@@ -13,8 +13,8 @@ public class FocusPoint_02_JavaDefineSimpleGenericClass {
         // Topic-01: 在java库中类型变量一般使用：E ==> 表示集合的元素类型  K V ==> 表示表的关键字与值的类型  T U S ==> 表示任意类型
         // Topic-02: 换句话说：泛型类可以看做普通类的工厂
         // 构造对象 - 带有构造器的普通类
-        Pair<String, Integer> demo = new Pair<>("ragnor", 18);
-        Pair<String, String> demo2 = new Pair<>();
+        RagnorPair<String, Integer> demo = new RagnorPair<>("ragnor", 18);
+        RagnorPair<String, String> demo2 = new RagnorPair<>();
         // 方法
         System.out.println(demo.getFirst());
         System.out.println(demo2.getSecond());
@@ -26,12 +26,12 @@ public class FocusPoint_02_JavaDefineSimpleGenericClass {
 /**一个泛型类（generic class）就是具有一个或多个类型变量的类
  * 我们只关注泛型，而不会为了数据存储的细节烦恼 */
 
-class Pair<T, U>{ // 类定义中的类型变量指定方法的返回类型以及域和局部变量的类型
+class RagnorPair<T, U>{ // 类定义中的类型变量指定方法的返回类型以及域和局部变量的类型
     private T first;  // 使用类型变量, 类型变量使用大写形式且短
     private U second;
 
-    public Pair(){first = null; second = null;}
-    public Pair(T first, U second) {this.first = first; this.second = second;}
+    public RagnorPair(){first = null; second = null;}
+    public RagnorPair(T first, U second) {this.first = first; this.second = second;}
 
     public T getFirst() { return first; }
     public U getSecond() { return second; }
---
+++ src/com/ragnor/corejava/chapter08_GenericProgramming/FocusPoint_03_JavaDefineGenericMethod.java
@@ -0,0 +1,39 @@
+package com.ragnor.corejava.chapter08_GenericProgramming;
+
+public class FocusPoint_03_JavaDefineGenericMethod {
+    public static void main(String[] args) {
+        DefineGenericMethod();
+    }
+
+    // FocusPoint-03：How to Define Generic Method ?
+    public static void DefineGenericMethod(){
+        // Topic-01: 泛型方法可以定义在普通类中，也可以定义在泛型类中
+        // Topic-02: 在调用一个泛型方法的时候，在方法名前的尖括号中放入具体的类型
+        String middle = RagnorDefineGenericMethod.<String>getMiddle("john", "Q", "public");
+
+        // Topic-03: 实际大多数情况下，方法调用中可以省略<String>类型参数，因为编译器有足够的信息能够推断出所调用的方法，
+        //           他通过传入的姓名的String[] 与泛型类型T[]进行匹配推断T一定是String
+        String middle2 = RagnorDefineGenericMethod.getMiddle("jod", "susan", "private");
+
+        // Topic-04: 少数情况下，编译器会报错 ==> 我们传入的是一个double + 2个 int对象 ==》编译器会自动打包参数为1个Double + 2个Integer
+        //                                  ==》找这些类的共同超类 ==》但是找到两个这样的超类型：Number 和 Comparable接口 ==》
+        //                                  说明其本身就是一个泛型类型，懂？
+
+        /*Error:(19, 61) java: 不兼容的类型: 推断类型不符合上限
+            推断: java.lang.Number&java.lang.Comparable<? extends java.lang.Number&java.lang.Comparable<?>>
+            上限: java.lang.Double,java.lang.Object
+         * */
+//        double middle3 = RagnorDefineGenericMethod.getMiddle(3.14, 1729, 0);
+        double middle3 = RagnorDefineGenericMethod.getMiddle(3.14, 1729.0, 0.1);
+
+        System.out.println(middle + " " + middle2 + " " + middle3);
+    }
+}
+
+
+class RagnorDefineGenericMethod{
+
+    public static <T> T getMiddle(T...a){  // 定义一个带有类型参数的简单方法
+        return a[a.length / 2];
+    }
+}
