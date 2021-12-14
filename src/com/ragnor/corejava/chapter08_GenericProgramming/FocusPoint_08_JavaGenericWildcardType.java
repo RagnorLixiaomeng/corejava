a605edc5d85352ae4232578f210bdf34de433a24
RagnorLixiaomeng <13155328119@163.com>
2021-12-14 16:36:00 +0800

    Complete Task-08_Java Generic Programming Design:FocusPoint_08_Java Generic Wildcard Type

---
+++ src/com/ragnor/corejava/chapter08_GenericProgramming/FocusPoint_08_JavaGenericWildcardType.java
@@ -0,0 +1,8 @@
+package com.ragnor.corejava.chapter08_GenericProgramming;
+
+public class FocusPoint_08_JavaGenericWildcardType {
+    public static void main(String[] args) {
+        // todo: 泛型类型中的通配符类型应用
+    }
+
+}
